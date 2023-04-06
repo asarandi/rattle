00a30b00959f73ecbd4a41af3e4f378cffdf4941
Alexandr SARANDI <sarandi.alex@gmail.com>
2023-04-06 12:56:58 -0700

    sawtooth, triangle, sine as audio callbacks

--- main.c
+++ main.c
@@ -2,51 +2,90 @@
 #include <stdint.h>
 #include <time.h>
 
+#define SFREQ 44100
+#define NOTE 110
+
 void square(void *userdata, Uint8 *stream, int len) {
+  (void)userdata;
   int16_t *ptr, i, v;
   static uint32_t ti;
   double steps;
 
-  (void)userdata;
-
-  steps = 44100.0 / 440.0;
-
+  steps = (double)SFREQ / (double)NOTE;
   ptr = (int16_t *)stream;
-  for (i = 0; i < len / 4; i++) {
+  for (i = 0; i < len / 2; i++) {
     v = ti > (steps / 2) ? -INT16_MAX : INT16_MAX;
     ti = (ti + 1) % (uint32_t)steps;
-    ptr[i * 2] = v;
-    ptr[i * 2 + 1] = v;
+    ptr[i] = v;
+  }
+}
+
+void triangle(void *userdata, Uint8 *stream, int len) {
+  (void)userdata;
+  int16_t *ptr, i, v;
+  static uint32_t ti;
+  double steps, qtr, inc;
+
+  steps = (double)SFREQ / (double)NOTE;
+  qtr = (double)SFREQ / (double)NOTE / 4.0;
+  inc = (double)INT16_MAX / qtr;
+
+  ptr = (int16_t *)stream;
+  for (i = 0; i < len / 2; i++) {
+    v = 0;
+    if (ti < qtr) {
+      v = inc * ti;
+    } else if (ti < 3 * qtr) {
+      v = INT16_MAX - (inc * (ti - qtr));
+    } else {
+      v = -INT16_MAX + (inc * (ti - (3 * qtr)));
+    }
+    ti = (ti + 1) % (uint32_t)steps;
+    ptr[i] = v;
+  }
+}
+
+void sawtooth(void *userdata, Uint8 *stream, int len) {
+  (void)userdata;
+  int16_t *ptr, i, v;
+  static uint32_t ti;
+  double steps, half, inc;
+
+  steps = (double)SFREQ / (double)NOTE;
+  half = (double)SFREQ / (double)NOTE / 2.0;
+  inc = (double)INT16_MAX / half;
+
+  ptr = (int16_t *)stream;
+  for (i = 0; i < len / 2; i++) {
+    v = ti < half ? 0 : -INT16_MAX;
+    v += ti < half ? inc * ti : inc * (ti - half);
+    ti = (ti + 1) % (uint32_t)steps;
+    ptr[i] = v;
   }
 }
 
 void sine(void *userdata, Uint8 *stream, int len) {
+  (void)userdata;
   int16_t *ptr, i, v;
   static uint32_t ti;
   double inc;
 
-  (void)userdata;
-
-  inc = (2.0 * M_PI) / (44100.0 / 440.0);
-
+  inc = (2.0 * M_PI) / ((double)SFREQ / (double)NOTE);
   ptr = (int16_t *)stream;
-  for (i = 0; i < len / 4; i++) {
+  for (i = 0; i < len / 2; i++) {
     v = sin(inc * ti++) * INT16_MAX;
-    ptr[i * 2] = v;
-    ptr[i * 2 + 1] = v;
+    ptr[i] = v;
   }
 }
 
 void noise(void *userdata, Uint8 *stream, int len) {
+  (void)userdata;
   int16_t *ptr, i, v;
 
-  (void)userdata;
-
   ptr = (int16_t *)stream;
-  for (i = 0; i < len / 4; i++) {
+  for (i = 0; i < len / 2; i++) {
     v = rand();
-    ptr[i * 2] = v;
-    ptr[i * 2 + 1] = v;
+    ptr[i] = v;
   }
 }
 
@@ -62,12 +101,12 @@ int main() {
   (void)SDL_memset(&desired, 0, sizeof(SDL_AudioSpec));
   (void)SDL_memset(&obtained, 0, sizeof(SDL_AudioSpec));
 
-  desired.freq = 44100;
+  desired.freq = SFREQ;
   desired.format = AUDIO_S16;
-  desired.channels = 2;
+  desired.channels = 1;
   desired.samples = 4096;
-  desired.size = 4096 * 2 * 2;
-  desired.callback = &sine;
+  desired.size = 4096 * 2 * 1;
+  desired.callback = &triangle;
 
   dev = SDL_OpenAudioDevice(NULL, 0, &desired, &obtained, 0);
   if (!dev) {
